281e3cd6b0572d1999374343b65076db4e04dc0c
Eric NICOLAS <Eric NICOLAS>
2016-01-19 01:29:33 +0100

    Removed 'capitalise' filter, since we're now gonna capitalise employees' firstnames in the database itself.

--- app/filters/customFilters.js
+++ app/filters/customFilters.js
@@ -5,15 +5,6 @@ var customFilters = require('angular').module('customFilters', []);
 var moment = require('moment');
 var _ = require('underscore');
 
-customFilters.filter('capitalise', function() {
-  return function(input, all) {
-    var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
-    return (!!input) ? input.replace(reg, function(txt){
-    	return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }) : '';
-  };
-});
-
 function summariseSite (site) {
   return site.site_name;
 }

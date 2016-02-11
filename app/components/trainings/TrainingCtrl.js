d8357f2bc581c486edd906fb8131b3d6317cafb4
ccjmne <ccjmne@gmail.com>
2016-02-11 02:17:01 +0100

    Added sign-in sheet generation for trainings.

--- app/components/trainings/TrainingCtrl.js
+++ app/components/trainings/TrainingCtrl.js
@@ -3,6 +3,7 @@
 
 var _ = require('underscore');
 var moment = require('moment');
+var pdfMake = require('pdfmake');
 
 module.exports = function ($scope, $rootScope, $routeParams, dataSvc, trngSvc, $location, ngDialog, busySvc) {
 	busySvc.busy();
@@ -17,6 +18,107 @@ module.exports = function ($scope, $rootScope, $routeParams, dataSvc, trngSvc, $
 		busySvc.done();
 		$scope.$apply();
 	});
+
+	$scope.generateSignInSheet = function () {
+		pdfMake.createPdf({
+			pageMargins: [40, 80, 40, 60],
+			header: {
+				text: [
+					$scope.trng.type.trty_name + ' - ' + moment($scope.trng.trng_date).format('dddd Do MMMM YYYY'),
+					{text: '\nFiche d\'émargement', style: 'em'}
+				],
+				alignment: 'center',
+				margin: [20, 20, 20, 10]
+			},
+			footer: function (currentPage, pageCount) {
+				return {
+					columns: [
+						{
+							width: '*',
+							text: [
+								'Fiche formation : ',
+								{text: $location.absUrl(), link: $location.absUrl(), decoration: 'underline', style: 'em'}
+							]
+						}, {
+							width: 'auto',
+							text: [
+								'page ',
+								{text: currentPage.toString(), style: 'em'},
+								' sur ',
+								{text: pageCount.toString(), style: 'em'}
+							],
+							alignment: 'right'
+						}
+					],
+					margin: [20, 20, 20, 0]
+				};
+			},
+		  content: [
+		  	{
+		  		text: $scope.trng.type.trty_name + ' - ' + moment($scope.trng.trng_date).format('dddd Do MMMM YYYY') + '\n\n\n',
+		  		style: 'title',
+		  		alignment: 'center'
+		  	}, {
+					style: 'tableExample',
+					table: {
+						headerRows: 1,
+						widths: ['auto', 'auto', 'auto', '*'],
+						body: [
+							[
+								{ text: 'Matricule', style: 'table-header'},
+								{ text: 'Nom', style: 'table-header'},
+								{ text: 'Prénom', style: 'table-header'},
+								{ text: 'Émargement', style: 'table-header', alignment: 'center'}
+							]
+						].concat(_.map(_.sortBy($scope.trainees, 'empl_surname'), function (empl) {
+							return [empl.empl_pk, {text: empl.empl_surname, style: 'em'}, {text: empl.empl_firstname, style: 'em'}, ''];
+						}))
+					},
+					layout: {
+						hLineWidth: function (i, node) {
+							if (i === 0 || i === node.table.body.length) {
+								return 0;
+							}
+
+							return (i === 1) ? 2 : 1;
+						},
+						vLineWidth: function () {
+							return 0;
+						},
+						hLineColor: function (i) {
+							return (i === 1) ? 'black' : 'grey';
+						},
+						vLineColor: function () {
+							return 'grey';
+						},
+						paddingTop: function (i) {
+							return i === 0 ? 4 : 20;
+						},
+						paddingBottom: function (i) {
+							return i === 0 ? 4 : 20;
+						},
+						paddingLeft: function (i) { return i === 0 ? 0 : 6; },
+						paddingRight: function (i, node) { return i === node.table.widths.length - 1 ? 0 : 6; }
+					}
+				}
+		  ],
+		  styles: {
+		  	'em': {
+		  		color: 'black'
+		  	},
+		  	'title': {
+					fontSize: 18
+		  	},
+		  	'table-header': {
+					bold: true,
+					fontSize: 13
+		  	}
+		  },
+		  defaultStyle: {
+				color: 'grey'
+		  }
+		}).open();
+	};
 
 	$scope.selectEmployee = function (empl_pk) {
 		$location.path('/employees/' + empl_pk);

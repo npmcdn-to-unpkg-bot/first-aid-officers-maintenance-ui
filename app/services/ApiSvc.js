b15b050198eaca45de25aa6fcf00295fec9eb568
ccjmne <ccjmne@gmail.com>
2016-02-17 16:04:10 +0100

    Set API base URL to the dev. environment's.

--- app/services/ApiSvc.js
+++ app/services/ApiSvc.js
@@ -9,7 +9,7 @@ module.exports = function ($http, $q) {
 		}
 	};
 	
-	apiSvc.apiBaseUrl = '//peidf.formationssecurite.fr/api/';
+	apiSvc.apiBaseUrl = 'http://localhost:8080/api/';
 
 	apiSvc.authEndpoint = apiSvc.apiBaseUrl + 'auth/';
 	apiSvc.adminEndpoint = apiSvc.apiBaseUrl + 'admin/';

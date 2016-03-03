88bf7e431f16b1ec634890311a99b3ac20b35bbc
ccjmne <ccjmne@gmail.com>
2016-03-03 20:13:29 +0100

    Now also displaying global range's data.

--- app/components/trainings/TrainingsStatsCtrl.js
+++ app/components/trainings/TrainingsStatsCtrl.js
@@ -11,10 +11,9 @@ module.exports = function ($scope, $rootScope, dataSvc, dateFilter, busySvc) {
     $scope.globalStats = results[0][0];
     $scope.stats = _(results[0]).map(function (stats, interval) {
       return { stats: stats, interval: parseInt(interval) };
-    }).reject(function (stats) {
-      return stats.interval === 0;
     }).orderBy('interval', 'desc').value();
-    $scope.columns = _.range(0, _.last($scope.stats).stats.length);
+    $scope.stats = [_.last($scope.stats)].concat(_.take($scope.stats, $scope.stats.length - 1));
+    $scope.columns = _.last($scope.stats).stats.length;
     var smallestInterval = _.last($scope.stats).interval;
     $scope.stats = _.map($scope.stats, function (stats) {
       return _.extend({ colSpan: stats.interval / smallestInterval }, stats);

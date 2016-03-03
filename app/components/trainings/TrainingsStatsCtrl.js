c50b78370e5f1ed9f2dbc5c5582c5c0f0053e6b4
ccjmne <ccjmne@gmail.com>
2016-03-03 22:17:08 +0100

    Beautified dates ranges display.

--- app/components/trainings/TrainingsStatsCtrl.js
+++ app/components/trainings/TrainingsStatsCtrl.js
@@ -15,6 +15,24 @@ module.exports = function ($scope, $rootScope, dataSvc, dateFilter, busySvc) {
     busySvc.done();
   });
 
+  $scope.getIntervalDisplay = function (interval, beginning, end) {
+    var _beginning = moment(beginning);
+    var _dateFilter;
+    if (interval === 0) {
+      _dateFilter = _.partialRight(dateFilter, 'fullDate')
+    } else if (interval === 12 && _beginning.dayOfYear() === 1) {
+      return dateFilter(beginning, 'yyyy');
+    } else if (interval === 1 && _beginning.date() === 1) {
+      return dateFilter(beginning, 'MMMM');
+    } else if (interval < 6) {
+      _dateFilter = _.partialRight(dateFilter, 'MMM')
+    } else {
+      _dateFilter = _.partialRight(dateFilter, 'MMM yyyy')
+    }
+
+    return _dateFilter(beginning) + ' - ' + _dateFilter(end);
+  };
+
   $scope.generate = function () {
     busySvc.busy();
     dataSvc.getTrainingsStats(dateFilter($scope.beginning, 'yyyy-MM-dd'), dateFilter($scope.end, 'yyyy-MM-dd'), [0].concat($scope.intervals)).then(function (results) {

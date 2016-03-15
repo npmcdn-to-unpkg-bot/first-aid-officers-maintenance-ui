cd54401893f72e50e62c14ac16be9a08f214cc5b
ccjmne <ccjmne@gmail.com>
2016-03-15 23:43:19 +0100

    Updated if-role directive: now supports multiple roles requirement. Also now only able to edit a training's completion record is the user is both 'trainer' AND 'admin'.

--- app/directives/ifRole.js
+++ app/directives/ifRole.js
@@ -1,27 +1,40 @@
 'use strict';
 
-var _ = require('underscore');
+var _ = require('lodash');
 
 module.exports = function ($rootScope, ngIfDirective) {
   var ngIf = ngIfDirective[0];
 
-  function link ($scope, element, attrs) {
+  function asdf(roles, role) {
+    if (_.startsWith(role, '!')) {
+      return !_.includes(roles, role.substr(1));
+    }
+
+    return _.includes(roles, role);
+  }
+
+  function link($scope, element, attrs) {
     var shown = false;
     attrs.ngIf = function () {
-	    return shown;
+      return shown;
     };
-    
+
     ngIf.link.apply(ngIf, arguments);
     $rootScope.$watch('currentUser.info.roles', function (roles) {
-	    if(attrs.ifRole.indexOf('!') === 0) {
-	      shown = !_.contains(roles, attrs.ifRole.substr(1));
+      if (_.isArray($scope.roles)) {
+        shown = _.every($scope.roles, function (role) {
+          return asdf(roles, role);
+        });
       } else {
-        shown = _.contains(roles, attrs.ifRole);
-	    }
+        shown = asdf(roles, $scope.roles);
+      }
     });
   }
 
   return {
+    scope: {
+      roles: '=ifRole'
+    },
     priority: ngIf.priority,
     restrict: ngIf.restrict,
     terminal: ngIf.terminal,

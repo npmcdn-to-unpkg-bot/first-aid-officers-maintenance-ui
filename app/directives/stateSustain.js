c70a547e8f7c1777baa4017ceaf4840116d44e0b
ccjmne <ccjmne@gmail.com>
2016-03-30 01:34:54 +0200

    Code beautifying.

--- app/directives/stateSustain.js
+++ app/directives/stateSustain.js
@@ -3,20 +3,19 @@
 var _ = require('underscore');
 
 module.exports = function ($rootScope, $cookies) {
-  function link ($scope) {
+  function link($scope) {
     var ready = false;
     $scope.$watch('items', function (items) {
-      if(ready) {
+      if (ready) {
         $cookies.putObject('stateSustain-' + $scope.stateId, items);
-      } else if(items) {
+      } else if (items) {
         ready = true;
-        var cookies = $cookies.getObject('stateSustain-' + $scope.stateId) || {};
         _.each($scope.items, function (item) {
-          var found = _.find(cookies, function (cookieItem) {
+          var found = _.find($cookies.getObject('stateSustain-' + $scope.stateId) || {}, function (cookieItem) {
             return cookieItem[$scope.itemsId] === item[$scope.itemsId];
           });
 
-          if(found) {
+          if (found) {
             item[$scope.state] = found[$scope.state];
           }
         });

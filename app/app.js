f71f5bbed517b15d16142da78d24bab1c478cecd
ccjmne <ccjmne@gmail.com>
2016-07-01 22:22:03 +0200

    Cleared last remains of smart-table.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ var angular = require('angular');
 var _ = require('lodash');
 require('angular-bootstrap-templates');
 require('angular-i18n-fr');
-require('angular-smart-table');
 require('angular-ui-sortable');
 require('bootstrap_material_design');
 require('bootstrap-switch');
@@ -36,18 +35,6 @@ $(function () {
     window.onresize = trianglify;
   }
 });
-angular.module('smart-table').run(['$templateCache', function ($templateCache) {
-  $templateCache.put('template/smart-table/pagination.html',
-    '    <nav ng-if="numPages && pages.length > 1">' +
-    '        <ul class="pagination">' +
-    '            <li ng-click="selectPage(1)" ng-class="{\'disabled\': currentPage == 1}"><a href="javascript:void(0)">&laquo;</a></li>' +
-    '            <li ng-click="selectPage(currentPage - 1)" ng-class="{\'disabled\': currentPage == 1}"><a href="javascript:void(0)">&lsaquo;</a></li>' +
-    '            <li ng-repeat="page in pages" ng-class="{\'active\': page == currentPage}" ng-click="selectPage(page)"><a href="javascript:void(0)">{{page}}</a></li>' +
-    '            <li ng-click="selectPage(currentPage + 1)" ng-class="{\'disabled\': currentPage == numPages}"><a href="javascript:void(0)">&rsaquo;</a></li>' +
-    '            <li ng-click="selectPage(numPages)" ng-class="{\'disabled\': currentPage == numPages}"><a href="javascript:void(0)">&raquo;</a></li>' +
-    '        </ul>' +
-    '    </nav>');
-}]);
 
 angular.module('faomaintenanceApp', [
     require('angular-cookies'),
@@ -59,8 +46,7 @@ angular.module('faomaintenanceApp', [
     require('ng-dialog'),
     'ngTable',
     'ui.bootstrap',
-    'ui.sortable',
-    'smart-table'
+    'ui.sortable'
   ]).config(['$routeProvider', 'ngDialogProvider', 'uibButtonConfig', '$httpProvider', '$provide',
     function ($routeProvider, ngDialogProvider, uibButtonConfig, $httpProvider, $provide) {
       $provide.decorator('orderByFilter', ['$delegate', '$parse', function ($delegate, $parse) {

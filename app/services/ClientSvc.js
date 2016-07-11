fad959b87b91da92e111cb3c5a98ae31a015f955
ccjmne <ccjmne@gmail.com>
2016-07-11 16:08:49 +0200

    More widely supported function to generate the client's base64 encoded logo (using a canvas).

--- app/services/ClientSvc.js
+++ app/services/ClientSvc.js
@@ -1,17 +1,32 @@
 'use strict';
 
-function toDataUrl(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'blob';
-  xhr.onload = function () {
-    var reader = new FileReader();
-    reader.onloadend = function () {
-      callback(reader.result);
-    }
-    reader.readAsDataURL(xhr.response);
+function toDataUrl(src, callback, outputFormat) {
+  // Create an Image object
+  var img = new Image();
+  // Add CORS approval to prevent a tainted canvas
+  img.crossOrigin = 'Anonymous';
+  img.onload = function () {
+    var canvas = document.createElement('CANVAS');
+    var ctx = canvas.getContext('2d');
+    var dataURL;
+    // Resize the canavas to the image dimensions
+    canvas.height = this.height;
+    canvas.width = this.width;
+    ctx.drawImage(this, 0, 0);
+    dataURL = canvas.toDataURL(outputFormat);
+    callback(dataURL);
+    // Mark for garbage collection
+    canvas = null;
   };
-  xhr.open('GET', url);
-  xhr.send();
+
+  img.src = src;
+  // make sure the load event fires for cached images too
+  if (img.complete || img.complete === undefined) {
+    // Flush cache
+    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+    // Try again
+    img.src = src;
+  }
 }
 
 module.exports = function ($q, apiSvc) {

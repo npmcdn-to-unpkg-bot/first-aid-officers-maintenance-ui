703fe9b7246cf26fb8ebaad432bfa032fa8e788c
ccjmne <ccjmne@gmail.com>
2016-07-12 00:01:10 +0200

    Fixed bug where the Client Management page would reload before the actual update would have gone through.

--- app/components/administration/client/ClientMgmtCtrl.js
+++ app/components/administration/client/ClientMgmtCtrl.js
@@ -11,21 +11,18 @@ module.exports = function ($scope, $route, clientSvc, busySvc, ngDialog) {
     busySvc.busy('clientUpdate', true);
     var promises = [];
     if ($scope.form.file) {
-      promises.concat([clientSvc.uploadLogo($scope.form.file)]);
+      promises = promises.concat([clientSvc.uploadLogo($scope.form.file)]);
     }
 
     if ($scope.form.$dirty) {
-      promises.concat([clientSvc.updateClientInfo($scope.client)]);
+      promises = promises.concat([clientSvc.updateClientInfo($scope.client)]);
     }
 
     Promise.all(promises).then(function () {
-      // Wait for an additional second before fetching the new logo from AWS S3
-      setTimeout(function () {
-        busySvc.done('clientUpdate');
-        $scope.$emit('clientUpdate');
-        $scope.$emit('alert', { type: 'success', msg: 'Configuration de la solution mise &agrave; jour.' });
-        $route.reload();
-      }, $scope.form.file ? 2000 : 0);
+      busySvc.done('clientUpdate');
+      $scope.$emit('clientUpdate');
+      $scope.$emit('alert', { type: 'success', msg: 'Configuration de la solution mise &agrave; jour.' });
+      $route.reload();
     }, _.partial($scope.$emit, 'error'));
   };
 };
